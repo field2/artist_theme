55e19c5e01d9187c48687b8230b69710ad62d037
Ben Dunkle <ben@field2.com>
2017-11-30 08:14:21 -0500

    added comments

---
+++ comments.php
@@ -0,0 +1,39 @@
+<div class="comments">
+	<?php if ( have_comments() ) : ?>
+			<h3 id="comments-title"><?php
+			printf( _n( 'One Response to %2$s', '%1$s Responses to %2$s', get_comments_number() ),
+			number_format_i18n( get_comments_number() ), '<em>' . get_the_title() . '</em>' );
+			?></h3>
+
+<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
+			<div class="navigation">
+				<div class="nav-previous"><?php previous_comments_link( __( '<span class="meta-nav">&larr;</span> Older Comments' ) ); ?></div>
+				<div class="nav-next"><?php next_comments_link( __( 'Newer Comments <span class="meta-nav">&rarr;</span>' ) ); ?></div>
+			</div> 
+<?php endif;  ?>
+
+			<ol class="commentlist">
+				<?php
+					wp_list_comments();
+				?>
+			</ol>
+
+<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) :  ?>
+			<div class="navigation">
+				<div class="nav-previous"><?php previous_comments_link( __( '<span class="meta-nav">&larr;</span> Older Comments' ) ); ?></div>
+				<div class="nav-next"><?php next_comments_link( __( 'Newer Comments <span class="meta-nav">&rarr;</span>' ) ); ?></div>
+			</div>
+<?php endif;  ?>
+
+<?php else : 
+	if ( ! comments_open() ) :
+?>
+	<?php _e( 'Comments are closed.' ); ?></p>
+    -->
+<?php endif; // end ! comments_open() ?>
+
+<?php endif; // end have_comments() ?>
+
+
+
+<?php comment_form(); ?>
