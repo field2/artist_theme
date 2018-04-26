aa996fc7e261e0ded0586f5679327ee40ea83df1
Ben Dunkle <ben@field2.com>
2018-04-26 10:42:46 -0400

    Full-height slideshow on front page

--- front-page.php
+++ front-page.php
@@ -1,27 +1,7 @@
 <?php get_header(); ?>
 
-
 <div class="content">
-<ul id="slideshow">
 
-
-
-<?php
-$slides = new WP_Query('post_type=slide'); 
-while ($slides->have_posts()) : $slides->the_post(); ?>
-<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $slides->ID ), 'slide' ); ?>
-	<li style="background-image: url('<?php echo $image[0]; ?>')"></li>
-<?php endwhile; ?>
-</ul>
-
-
-	
-<div id="controls">
-<div id="prev"><i class="fa fa-caret-left"></i><i class="fa fa-caret-left"></i></div><!-- #prev  -->
-&nbsp;&nbsp;
-<div id="next"><i class="fa fa-caret-right"></i><i class="fa fa-caret-right"></i></div><!-- #next  -->
-
-</div><!-- #controls -->
 <div class="main">
 <?php 	if ( have_posts() ) : 	while ( have_posts() ) : 	the_post(); ?> 
 <?php 	the_title('<h2>','</h2>'); 	the_content(); ?>
--- header.php
+++ header.php
@@ -25,11 +25,11 @@ $logo = esc_url(get_theme_mod('eol_logo'));
 	?>
 </head>
 <body <?php body_class(); ?>>
-
+	<div class="wrapper">
 <header><div class="hamburger hamburger--3dx">
-    <div class="hamburger-box">
-      <div class="hamburger-inner"></div>
-    </div>
+	<div class="hamburger-box">
+	  <div class="hamburger-inner"></div>
+	</div>
   </div>
 	<h1><?php 
 	bloginfo('name'); 
@@ -38,3 +38,22 @@ $logo = esc_url(get_theme_mod('eol_logo'));
 <?php 
 	wp_nav_menu(array('theme_location'=> 'primary', 'container'=>'nav')); 
 ?>
+
+<ul id="slideshow">
+
+
+
+<?php
+$slides = new WP_Query('post_type=slide'); 
+while ($slides->have_posts()) : $slides->the_post(); ?>
+<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $slides->ID ), 'slide' ); ?>
+	<li style="background-image: url('<?php echo $image[0]; ?>')"></li>
+<?php endwhile; ?>
+</ul>
+<div id="controls">
+<div id="prev"><i class="fa fa-caret-left"></i><i class="fa fa-caret-left"></i></div><!-- #prev  -->
+&nbsp;&nbsp;
+<div id="next"><i class="fa fa-caret-right"></i><i class="fa fa-caret-right"></i></div><!-- #next  -->
+
+</div><!-- #controls -->
+</div><!--  /.wrapper -->
