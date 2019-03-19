0a9717d5ba990c4c22a1d9e0569d247ca56a286e
Ben Dunkle <ben@field2.com>
2019-03-19 08:55:09 -0400

    worked grid into galleries

--- front-page.php
+++ front-page.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
 
-<div id="main">
+<main id="main">
 
 <?php 	if ( have_posts() ) : 	while ( have_posts() ) : 	the_post(); ?> 
 <?php 	the_content(); ?>
 <?php 	endwhile; 	else : ?>
 	<p>Nothing to see here, folks.</p>
 <?php 	endif; ?>
-</div><!--  /#main -->
+</main><!--  /#main -->
 
 <?php get_footer(); ?>
